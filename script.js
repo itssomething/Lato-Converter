bc1421f3aad3b6e8686f0a032d20299475197154
Manh <vdmanh107@gmail.com>
2017-12-26 13:24:28 +0700

    added copy function

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function getText(){
     var output = document.getElementById('input').value;
     document.getElementById('output').innerHTML = output;
 }
+function copyFunction() {
+  var copyText = document.getElementById("output");
+  copyText.select();
+  document.execCommand("Copy");
+  alert("Copied the text");
+}
